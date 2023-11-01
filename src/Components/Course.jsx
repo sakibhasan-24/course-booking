3521e105c99847d94c49ee4cd586d4b0af85209c
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-11-01 19:46:18 +0600

    design Single card

--- src/Components/Course.jsx
+++ src/Components/Course.jsx
@@ -2,21 +2,39 @@ export default function Course({ course }) {
   console.log(course);
   return (
     <div className="w-1/2 p-2 border-2">
-      <div>
+      <div className="w-full mx-auto border-4">
         <img
-          className="w-[400px] rounded-lg"
+          className="w-full h-[300px] rounded-3xl p-2"
           src={course.courseImg}
           alt="img"
         />
       </div>
-      <div>
-        <h2 className="text-3xl">{course.courseName}</h2>
-        <h3 className="text-2xl">{course.text}</h3>
-        <p className="text-3xl">
-          {" "}
-          $ <span className="text-4xl">{course.price}</span>
+      <div className="m-3 p-1 ">
+        <h2 className="text-3xl text-center font-extrabold tracking-wide text-gray-600">
+          {course.courseName}
+        </h2>
+        <h3 className="text-xl text-center my-3 text-gray-700">
+          {course.text}
+        </h3>
+        <div className="flex items-center justify-between">
+          <p className="text-3xl font-extralight">
+            {" "}
+            ${" "}
+            <span className="text-4xl font-extrabold text-indigo-950">
+              {course.price}
+            </span>
+          </p>
+          <p className="text-2xl font-bold text-gray-500">
+            Credit <span className="text-3xl">{course.creditHour}</span> hr
+          </p>
+        </div>
+        <p className="text-xl text-blue-950 font-bold my-4">
+          <span className="text-xs"> instructor:</span>
+          {course.instructorName}
         </p>
-        <p>Credit {course.creditHour} h</p>
+        <button className="bg-green-800 text-yellow-50 border-0 py-2 w-full cursor-pointer px-8 rounded-xl text-2xl">
+          Enroll
+        </button>
       </div>
     </div>
   );
